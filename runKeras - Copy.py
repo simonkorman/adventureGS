fbffbf879124da4cdc5ddd871f72c057793c8b76
simonkorman <simon.korman@gmail.com>
2017-02-19 11:55:23 +0200

    deleting keras copy

--- runKeras - Copy.py
+++
@@ -1,9 +0,0 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-###########
-model = Sequential()
-model.add(Dense(output_dim=64, input_dim=100))
-model.add(Activation("relu"))
-model.add(Dense(output_dim=10))
-model.add(Activation("softmax"))
-#
